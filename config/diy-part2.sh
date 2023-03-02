458529dffff6e6e65686ef7026df7a09151ac6f5
cxh <musheng@baofu.com>
2023-03-02 16:52:08 +0800

    自定义

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -10,15 +10,14 @@ sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generat
 # Remove packages
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/packages/net/haproxy
-rm -rf ./feeds/packages/net/mosdns
 rm -rf ./feeds/packages/net/v2ray-geodata
 rm -rf ./package/feeds/luci/luci-app-passwall
 rm -rf ./package/feeds/packages/haproxy
-rm -rf ./package/feeds/packages/mosdns
 rm -rf ./package/feeds/packages/v2ray-geodata
 
 # Add packages
-svn co https://github.com/nantayo/passwall/trunk package/passwall
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
-svn co https://github.com/sbwml/luci-app-mosdns/trunk package/mosdns
 svn co https://github.com/sbwml/v2ray-geodata/trunk package/geodata
+
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
